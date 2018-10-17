ff705312655c1d1be7754c4a57220af0187cdff2
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2018-10-17 18:36:12 +0530

    Initial Files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+import "./App.css";
+import HomeComponent from './Home';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+       <HomeComponent /> 
+        <h1> Hello, World! </h1>
+       
+      </div>
+    );
+  }
+}
+
+export default App; 
