ef33fde97e2aeb20a9de70b708fcc76e43dba1f4
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2018-12-24 18:40:49 +0530

    added  server side rendering to react app

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,8 +1,12 @@
 import React, { Component} from "react";
 import "./App.css";
-import HomeComponent from './Home';
+import HomeComponent from './Home.jsx';
 
 class App extends Component{
+  componentDidMount(){
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    console.log(document.location.hostname);
+  }
   render(){
     return(
       <div className="App">
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,74 @@
-const path = require("path");
 const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+
+const browserConfig = {
+  entry: "./src/javascript/index.js",
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  devtool: "cheap-module-source-map",
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: "file-loader",
+        options: {
+          name: "public/media/[name].[ext]",
+          publicPath: url => url.replace(/public/, "")
+        }
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: { presets: ["react-app"] }
       }
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
+
+const serverConfig = {
+  entry: "./src/server/index.js",
+  target: "node",
+  output: {
+    path: __dirname,
+    filename: "server.js",
+    libraryTarget: "commonjs2"
+  },
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: "file-loader",
+        options: {
+          name: "public/media/[name].[ext]",
+          publicPath: url => url.replace(/public/, ""),
+          emit: false
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader/locals"
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: { presets: ["react-app"] }
+      }
+    ]
+  }
+};
+
+module.exports = [browserConfig, serverConfig];
